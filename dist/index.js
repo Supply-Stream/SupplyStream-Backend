0539e8df2d26baf817e9afef4e2fd22e14755296
Max Mandia <mmandia511@gmail.com>
2023-05-16 11:03:40 -0400

    build: updated build

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-console.log("hey!");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port 3001");
+});
 //# sourceMappingURL=index.js.map
