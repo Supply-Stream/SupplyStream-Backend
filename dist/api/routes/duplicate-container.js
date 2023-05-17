6e30f90666d6affbe945051acc483d99c1554eb6
Max Mandia <mmandia511@gmail.com>
2023-05-17 14:04:30 -0400

    feat: added duplicate container endpoint

--- dist/api/routes/duplicate-container.js
+++ dist/api/routes/duplicate-container.js
@@ -4,13 +4,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const firebase_1 = __importDefault(require("../../config/firebase"));
+const add_container_1 = __importDefault(require("../helpers/add-container"));
 const router = express_1.default.Router();
 router.get("/", async (req, res) => {
-    //   let respy = await getMSCEvents("MSDU2774304");
-    //   let respy = await getZimEvents("TCNU6836631");
-    //   let respy = await getMaerskEvents("TCKU1122133");
-    //   let respy = await getOneEvents("BMOU1484792");
-    //   let respy = await getCoscoEvents("FTAU1498080");
+    const oldContainerID = req.query.oldContainerID;
+    const newContainerID = req.query.newContainerID;
+    const shippingLine = req.query.shippingLine;
+    if (!newContainerID || !newContainerID || !shippingLine) {
+        res.status(400).send({ error: "missing containerID or newContainerID" });
+        return;
+    }
+    let checkIfContainerExists = await firebase_1.default
+        .firestore()
+        .collection("containers")
+        .doc(newContainerID)
+        .get();
+    if (checkIfContainerExists.exists) {
+        res.status(409).send({ error: "container already exists" });
+        return;
+    }
+    let OG_CONTAINER = await firebase_1.default
+        .firestore()
+        .collection("containers")
+        .doc(oldContainerID)
+        .get();
+    let OG_CONTAINER_DATA = OG_CONTAINER.data();
+    OG_CONTAINER_DATA["template"]["CONTAINER"] = newContainerID;
+    OG_CONTAINER_DATA["template"]["SHIPPING LINE"] = shippingLine;
+    OG_CONTAINER["shippingLine"] = shippingLine;
+    OG_CONTAINER["dateAdded"] = new Date();
+    await firebase_1.default
+        .firestore()
+        .collection("containers")
+        .doc(newContainerID)
+        .set(OG_CONTAINER_DATA);
+    await (0, add_container_1.default)(shippingLine, newContainerID, true);
 });
 exports.default = router;
 //# sourceMappingURL=duplicate-container.js.map
--- src/api/routes/duplicate-container.ts
+++ src/api/routes/duplicate-container.ts
@@ -1,15 +1,49 @@
 import express, { Request, Response } from "express";
 import admin from "../../config/firebase";
 import { client } from "../../config/axiom";
-
+import addContainer from "../helpers/add-container";
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  //   let respy = await getMSCEvents("MSDU2774304");
-  //   let respy = await getZimEvents("TCNU6836631");
-  //   let respy = await getMaerskEvents("TCKU1122133");
-  //   let respy = await getOneEvents("BMOU1484792");
-  //   let respy = await getCoscoEvents("FTAU1498080");
+  const oldContainerID = req.query.oldContainerID as string;
+  const newContainerID = req.query.newContainerID as string;
+  const shippingLine = req.query.shippingLine as string;
+
+  if (!newContainerID || !newContainerID || !shippingLine) {
+    res.status(400).send({ error: "missing containerID or newContainerID" });
+    return;
+  }
+
+  let checkIfContainerExists = await admin
+    .firestore()
+    .collection("containers")
+    .doc(newContainerID)
+    .get();
+
+  if (checkIfContainerExists.exists) {
+    res.status(409).send({ error: "container already exists" });
+    return;
+  }
+
+  let OG_CONTAINER = await admin
+    .firestore()
+    .collection("containers")
+    .doc(oldContainerID)
+    .get();
+  let OG_CONTAINER_DATA = OG_CONTAINER.data();
+
+  OG_CONTAINER_DATA["template"]["CONTAINER"] = newContainerID;
+  OG_CONTAINER_DATA["template"]["SHIPPING LINE"] = shippingLine;
+  OG_CONTAINER["shippingLine"] = shippingLine;
+  OG_CONTAINER["dateAdded"] = new Date();
+
+  await admin
+    .firestore()
+    .collection("containers")
+    .doc(newContainerID)
+    .set(OG_CONTAINER_DATA);
+
+  await addContainer(shippingLine, newContainerID, true);
 });
 
 export default router;
