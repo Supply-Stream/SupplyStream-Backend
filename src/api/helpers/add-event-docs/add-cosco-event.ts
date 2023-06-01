42b93db3e2c54679e34d350d17436a5c2fa692eb
MaxMandia <mmandia511@gmail.com>
2023-06-01 18:54:03 -0400

    feat: new

--- src/api/helpers/add-event-docs/add-cosco-event.ts
+++ src/api/helpers/add-event-docs/add-cosco-event.ts
@@ -19,7 +19,7 @@ export default async function addCoscoEvent(
         eventClassifierCode: "ACT",
       },
     });
-
+  //
   let containerDoc = await admin
     .firestore()
     .collection("containers")
